d6c0967dc0990b14ef7287b2570e83eba580e148
Askhad Arzybaev <aarzybaev@gmail.com>
2024-02-02 10:04:47 +0600

    created Appbar component

---
+++ src/components/Appbar/Appbar.tsx
@@ -0,0 +1,12 @@
+
+const Appbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container-fluid">
+        <span className="navbar-brand">HW-61</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Appbar;
